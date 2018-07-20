c68d258d91e0ffc4d5c082fe51585543cc6ceb39
hirotaka.s <hiro.0723boss.macpro@gmail.com>
2018-07-20 19:19:03 +0900

    [add] UILbelの変更

--- FirstKadaiApp/ViewController.swift
+++ FirstKadaiApp/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        ラベルの作成
+        let label = UILabel()
+//        文字の挿入
+        label.text = "HelloWorld!!"
+//        フォントサイズの変更
+        label.font = UIFont(name: "HiraMinProN-W3", size: 30)
+//        サイズの自動調整
+        label.sizeToFit()
+//        中央揃え
+        label.center = self.view.center
+//        ViewにLabelを追加
+        self.view.addSubview(label)
+//        文字のカラー変更
+        label.textColor = UIColor.green
+        
     }
 
     override func didReceiveMemoryWarning() {
